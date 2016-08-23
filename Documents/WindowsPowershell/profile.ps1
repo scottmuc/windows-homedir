f0ac3fe30cfd52eca7322430a2c36d996c09bf58
Scott Muc <scottmuc@gmail.com>
2016-08-23 22:15:53 +0100

    Added PowerShell profile

---
+++ Documents/WindowsPowershell/profile.ps1
@@ -0,0 +1,6 @@
+function which($name) { Get-Command $name | Select-Object Definition }
+function rm-rf($item) { Remove-Item $item -Recurse -Force }
+function touch($file) { "" | Out-File $file -Encoding ASCII }
+
+$UserBinDir = "$($env:UserProfile)\bin"
+$env:Path += ";$($UserBinDir)"
